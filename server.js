4f1ab1d865f9223ba2a5da5c41e5783f9be73015
Nathan Clarke <clarken1996@gmail.com>
2018-01-25 12:54:44 +1100

    added rojects page

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
+    welcome: 'Portfolio goes here'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Failed to load..'
